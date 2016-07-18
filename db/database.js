9a0e2ad50a0c86725c00993df472947f8f92316d
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-18 11:34:54 -0500

    db connect/error/disconnect message

--- db/database.js
+++ db/database.js
@@ -3,3 +3,16 @@ var mongoose = require('mongoose');
 //DB connection
 var connectionString = process.env.DB;
 console.log('Attempting to Connect');
+
+mongoose.connect(connectionString);
+// NOTE: Connect to DB in connectionString
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to: ' + process.env.DB);
+});
+mongoose.connection.on('error', function (error) {
+    console.log('Mongoose ERROR ' + error);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected from: ' + process.env.DB);
+});
