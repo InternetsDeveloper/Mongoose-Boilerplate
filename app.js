425496609fb3a347b4ecf7d9c12bfcb05fe77627
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-18 11:43:15 -0500

    db load

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+//NOTE: Load DB before Controllers 
+require('./db/database');
+
+var routes = require('./controllers/index');
+
+var app = express();
