b143c988a57dc2879f3c52acb497ccbd21d305aa
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-18 11:38:02 -0500

    rq parsers

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon')
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
