bcede91fb0d8956eb3f6eeec067b54bf204c4283
InternetsDeveloper <a.guy06@yahoo.com>
2016-07-18 11:56:09 -0500

    404 forward

--- app.js
+++ app.js
@@ -16,3 +16,23 @@ var app = express();
 //NOTE: Engine Setup / View
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+//NOTES: Favicon - /public 
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+//NOTES: Forward 404 to Error Handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
